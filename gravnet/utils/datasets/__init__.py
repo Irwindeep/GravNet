38ad907522e8d6dfd46f37ce8996e6903f165436
Irwindeep <b22ai022@iitj.ac.in>
2025-03-28 10:51:17 +0530

    Update datasets

--- gravnet/utils/datasets/__init__.py
+++ gravnet/utils/datasets/__init__.py
@@ -1,7 +1,11 @@
 from .gw_dataset import GWDataset
 from .param_est import ParamEstData
+from .noise_seg import NoiseSegData
+from .wave_det import WaveDetData
 
 __all__ = [
     "GWDataset",
-    "ParamEstData"
+    "NoiseSegData",
+    "ParamEstData",
+    "WaveDetData"
 ]
